94f24b2875e625ab483e40acd6d25d48ecb036a1
Sławomir P <70092402+SlawomirP@users.noreply.github.com>
2022-05-17 17:08:33 +0200

    Add files via upload

--- javaUdemy/Loops.java
+++ javaUdemy/Loops.java
@@ -5,13 +5,26 @@ public class Loops {
 
         // Numbers 0-->30, 30-->0
         int amountNumbers = 30;
+        System.out.println("Task 1");
 
         for(int i = 0; i <= amountNumbers; i++){
            System.out.print(i + " ");
         }
         System.out.println();
+        System.out.println("Task 2");
         for (int i = amountNumbers; i >= 0; i--){
             System.out.print(i + " ");
         }
+        System.out.println();
+        System.out.println("Task 3");
+
+        // Only even from 0-30
+
+        for(int i = 0; i <= amountNumbers; i++){
+            if(i % 2 == 0)
+            System.out.print(i + " ");
+        }
+
+        // 10 elements array 
     }
 }

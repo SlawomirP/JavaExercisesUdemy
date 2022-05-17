2c0dc32b65a37149c09189422157b662b4d7fed5
Sławomir P <70092402+SlawomirP@users.noreply.github.com>
2022-05-17 09:42:27 +0200

    Add files via upload

--- javaUdemy/BonusExercises.java
+++ javaUdemy/BonusExercises.java
@@ -1,5 +1,21 @@
 package javaUdemy;
 
+import java.util.Random;
+
 public class BonusExercises {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        //Zad1
+        int arraySize = 5;
+        Random r = new Random();
+        int addResult = 0;
+
+        int [] intArray = new int[arraySize];
+
+        for(int i = 0; i < arraySize; i++){
+            System.out.print("Current value of addition: " + addResult + " ");
+            intArray[i] = r.nextInt(101) + 1;
+            System.out.println("Array index: " + i + ", value:  " + intArray[i]);
+            addResult += intArray[i];
+        }
+    }
 }

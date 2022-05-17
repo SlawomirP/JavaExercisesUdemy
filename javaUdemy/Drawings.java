8e2fb4bca151fedc79840c733df4cd706894dd9b
Sławomir P <70092402+SlawomirP@users.noreply.github.com>
2022-05-17 19:57:37 +0200

    Add files via upload

---
+++ javaUdemy/Drawings.java
@@ -0,0 +1,34 @@
+package javaUdemy;
+
+public class Drawings {
+    public static void main(String[] args){
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+        System.out.println("Task 1");
+        int stars = 5;
+        for(int i = 1; i <= stars; i++) {
+            for(int j = 1; j <= i; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("Task 2");
+//                         *
+//                        **
+//                       ***
+//                      ****
+//                     *****
+        for(int i = 1; i <= stars; i++ ){
+            for(int j = stars; j >= i; j--){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

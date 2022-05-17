19e2595826fd1e6c73fdd86f62257428b7c7142c
Sławomir P <70092402+SlawomirP@users.noreply.github.com>
2022-05-17 10:32:15 +0200

    Add files via upload

--- javaUdemy/BonusExercises.java
+++ javaUdemy/BonusExercises.java
@@ -4,7 +4,9 @@ import java.util.Random;
 
 public class BonusExercises {
     public static void main(String[] args){
-        //Zad1
+
+        //Zad1 tablice 5 elementowa uzupelnić dowolnie i zsumować
+
         int arraySize = 5;
         Random r = new Random();
         int addResult = 0;
@@ -16,6 +18,26 @@ public class BonusExercises {
             intArray[i] = r.nextInt(101) + 1;
             System.out.println("Array index: " + i + ", value:  " + intArray[i]);
             addResult += intArray[i];
+        }
+        System.out.println("----------------------------");
+
+        // Zad 2 stworzyc tablice dwuwymiarowa 3x5 double, zsumować wiersze i wypisać
+
+        int rows = 3;
+        int columns = 5;
+        double addResultDouble = 0;
+
+        double[][] doubleArray = new double [rows][columns];
+
+        for(int i = 0; i < rows; i++){
+            System.out.print("row " + i + " --> ");
+            for(int j = 0; j < columns; j++){
+                doubleArray[i][j] = r.nextDouble();
+                addResultDouble += doubleArray[i][j];
+                System.out.print(" " + doubleArray[i][j]);
+            }
+            System.out.println(" --> addiction result is: " + addResultDouble);
+            System.out.println();
         }
     }
 }
